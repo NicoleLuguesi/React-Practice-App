753021c8c344c21b1c178e850adc4895947267cd
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-04-15 19:38:44 -0400

    Prop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet name="Bruce" heroName="Batman" >
+          <p>This is children props</p>
+        </Greet>Greet>
+        <Greet name="Clark" heroName="Superman"/>
+        <button>Action</button>
+        <Greet name="Diana" heroName="Wonderwoman"/>
+        <Welcome name="Bruce" heroName="Batman"/>
+        <Welcome name="Clark" heroName="Superman"/>
+        <Welcome name="Diana" heroName="Wonderwoman"/>
+        <Hello />
+      </div>
+    );
+   }
+  }
 
 export default App;
+
