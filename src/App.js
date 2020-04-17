8790b1f3288fa4cc6d2bcc4533e54d751c2e9fe4
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-04-17 18:09:12 -0400

    state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message'
+import Counter from './components/Counter'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name="Bruce" heroName="Batman" >
+      <Counter />
+      {/* <Message /> */}
+        {/* <Greet name="Bruce" heroName="Batman" >
           <p>This is children props</p>
         </Greet>Greet>
         <Greet name="Clark" heroName="Superman"/>
@@ -18,7 +22,7 @@ class App extends Component {
         <Welcome name="Bruce" heroName="Batman"/>
         <Welcome name="Clark" heroName="Superman"/>
         <Welcome name="Diana" heroName="Wonderwoman"/>
-        <Hello />
+        <Hello /> */}
       </div>
     );
    }
